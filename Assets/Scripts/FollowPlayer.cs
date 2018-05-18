ea65adff2296c64eb2a544f017b0b5c96cf38f80
@Vika@ <38578416+workTask@users.noreply.github.com>
2018-05-18 12:33:47 +0300

    add FollowPlayer

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+	public Transform player;
+
+	void Start () {
+		Debug.Log("Hello FollowPlayer");
+	}
+	
+	
+	void Update () {
+		Debug.Log("player.position"+player.position);
+		Debug.Log("transform.position "+transform.position);
+		
+		transform.position = player.position;
+	}
+}
