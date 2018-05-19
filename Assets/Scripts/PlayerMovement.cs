40f2133ded44c2ccd17920fc18a0666aecced535
@Vika@ <38578416+workTask@users.noreply.github.com>
2018-05-19 15:36:23 +0300

    add fourth parametr ForceMode.VelocityChange

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -24,11 +24,11 @@ public class PlayerMovement: MonoBehaviour
 
 		if (Input.GetKey("d"))
 		{
-			rb.AddForce(sidewaysForce*Time.deltaTime, 0, 0);
+			rb.AddForce(sidewaysForce*Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 		}
 		if (Input.GetKey("a"))
 		{
-			rb.AddForce(-sidewaysForce*Time.deltaTime, 0, 0);
+			rb.AddForce(-sidewaysForce*Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 		}
 	}
 }
