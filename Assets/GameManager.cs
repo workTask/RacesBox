05666a9db9247efc6924f0708b8553e4a2bfe92e
@Vika@ <frantsuhvika@gmail.com>
2018-05-20 21:30:38 +0300

    add CollisionPlayer

---
+++ Assets/GameManager.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public void EndGame()
+	{
+		Debug.Log("Game OVER");
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
